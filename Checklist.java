dc21ff8530c77103df0abdbe18e0a434e44541b9
Loc <52533174+locpham246@users.noreply.github.com>
2023-03-21 17:54:01 -0500

    Update Checklist.java

--- Checklist.java
+++ Checklist.java
@@ -8,25 +8,29 @@ public class Checklist
     public Checklist(String listName)
     {
         this.listName = listName;
+        tasks = new Arraylist(Task);
     }
     
     public void displayList()
     {
-        
+        for (int i = 0; i < tasks.size(); i++) {
+            
+            }
+        }
     }
     
     public void pushTask()
     {
-        
+        tasks.add(task);  
     }
     
     public void removeTask()
     {
-        
+        tasks.remove(task);
     }
     
     public String listToString()
-    {
+   
         return "string";
     }
 }
