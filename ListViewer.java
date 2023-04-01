1e605d0a45673047c8e54e5abef9a82bb2923607
SethDMill <124477422+SethDMill@users.noreply.github.com>
2023-04-01 15:20:45 -0500

    Update ListViewer.java

--- ListViewer.java
+++ ListViewer.java
@@ -1,4 +1,68 @@
+import java.awt.EventQueue;
 
-public class ListViewer {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.AbstractListModel;
+import java.awt.Color;
+import javax.swing.ListSelectionModel;
+import javax.swing.JComboBox;
 
+public class ListViewer extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ListViewer frame = new ListViewer();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ListViewer() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Choose a list:");
+		lblNewLabel.setBounds(10, 11, 97, 14);
+		contentPane.add(lblNewLabel);
+
+		JList list = new JList();
+        list.setModel(new AbstractListModel() {
+            String[] values = new String[] {"List 1", "List 2", "List 3"};
+            public int getSize() {
+                return values.length;
+            }
+            public Object getElementAt(int index) {
+                return values[index];
+            }
+        });
+        list.setBounds(106, 35, 86, 143);
+        contentPane.add(list);
+        
+        JList list_1 = new JList();
+        list_1.setBounds(218, 35, 128, 143);
+        contentPane.add(list_1);
+
+
+	}
 }
